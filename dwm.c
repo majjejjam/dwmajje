a221c1498c8a8f1b1250c0339530c749a12b5e2d
Miró <miro.allard@gmail.com>
2020-09-05 13:27:09 +0200

    fixed bug

--- dwm.c
+++ dwm.c
@@ -304,8 +304,6 @@ applyrules(Client *c)
 	XClassHint ch = { NULL, NULL };
 
 	/* rule matching */
-	c->isterminal = r->isterminal;
-	c->noswallow  = r->noswallow;
 	c->isfloating = 0;
 	c->tags = 0;
 	XGetClassHint(dpy, c->win, &ch);
@@ -318,6 +316,8 @@ applyrules(Client *c)
 		&& (!r->class || strstr(class, r->class))
 		&& (!r->instance || strstr(instance, r->instance)))
 		{
+			c->isterminal = r->isterminal;
+			c->noswallow  = r->noswallow;
 			c->isfloating = r->isfloating;
 			c->tags |= r->tags;
 			for (m = mons; m && m->num != r->monitor; m = m->next);
